e14175fe333b4c4aedf4eb6209098564dcac1be5
jiaforios <jiaforios@163.com>
2016-12-07 09:14:45 +0800

    Initial Commit

---
+++ chartTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chartTest
+//
+//  Created by foscom on 16/12/7.
+//  Copyright © 2016年 zengjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chartTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chartTest
+//
+//  Created by foscom on 16/12/7.
+//  Copyright © 2016年 zengjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
